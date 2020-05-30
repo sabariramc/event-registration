a3704435fecd3cb545abaf57dbf82ea150ea983b
Sabariram <c.sabariram@gmail.com>
2020-05-30 21:13:29 +0530

    code reformatting

--- event/settings/settings.py
+++ event/settings/settings.py
@@ -2,5 +2,4 @@ import os
 
 from .helper import fix_assets_path
 
-STATIC_ASSETS_PATH = fix_assets_path(os.environ.get(
-    "EVENT_FIXED_ASSET_PATH", "../client/build"))
+STATIC_ASSETS_PATH = fix_assets_path(os.environ.get("EVENT_FIXED_ASSET_PATH", "../client/build"))
